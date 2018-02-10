26f07ac7f4bfaf10d040ec9e470e03dec044d36d
Tribhuwan Kushwaha <ktribhuwan@outlook.com>
2018-02-11 00:28:36 +0545

    Unit Test project added

---
+++ RestBankingService.Test/AccountServiceTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RestBankingService.Test
+{
+    [TestClass]
+    public class AccountServiceTest
+    {
+        [TestMethod]
+        public void Test_Deposit()
+        {
+            //Arrage
+            //Act
+            //Assert
+
+        }
+        [TestMethod]
+        public void Test_Withdraw()
+        {
+            //Arrage
+            //Act
+            //Assert
+
+        }
+        [TestMethod]
+        public void Test_Balance()
+        {
+            //Arrage
+            int accountNumber = 1234;
+            decimal actualValue = 0;
+            //Act
+            var service = new Implementation.AccountService();
+            var result=service.Balance(accountNumber);
+
+            //Assert
+            Assert.AreEqual(actualValue, result);
+        }
+    }
+}
---
+++ RestBankingService.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RestBankingService.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RestBankingService.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1c6207b5-318c-49c2-9294-12014f455bd4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
