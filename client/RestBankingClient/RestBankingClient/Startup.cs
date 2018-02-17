01526547fba75f6319c6b7ddadd6855bb5cdd192
Tribhuwan Kushwaha <ktribhuwan@outlook.com>
2018-02-18 01:38:39 +0545

    directory changed

---
+++ client/RestBankingClient/RestBankingClient/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RestBankingClient.Startup))]
+namespace RestBankingClient
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ client/RestBankingClient/RestBankingConsoleClient/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using RestBankingConsoleClient.AcountService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace RestBankingConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new AccountServiceClient();
+
+
+
+            var request = new RestBankingClient.RestBankingService.AccountRequest() { AccountNumber = 1234, Amount = 40, Currency = "USD" };
+            
+
+            var response=service.Withdraw(request);
+            Console.ReadLine();
+        }
+    }
+}
